55136213f72a77e77cfd039a8dea16d914511b1f
-- <-->
2022-05-10 19:44:07 +0300

    DB added

---
+++ src/main/java/com/example/sweater/Application.java
@@ -0,0 +1,17 @@
+/*
+https://www.youtube.com/watch?v=jH17YkBTpI4&list=PLU2ftbIeotGpAYRP9Iv2KLIwK36-o_qYk
+ */
+
+package com.example.sweater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
