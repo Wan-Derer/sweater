8c78b038608c265291949ca3703ffe02a807e53b
-- <-->
2022-05-20 09:54:56 +0300

    Add Bootstrap for UI

--- src/main/java/com/example/sweater/domain/User.java
+++ src/main/java/com/example/sweater/domain/User.java
@@ -28,6 +28,9 @@ public class User implements UserDetails {
   @Enumerated(EnumType.STRING)
   private Set<Role> roles;
 
+  public boolean isAdmin(){
+    return roles.contains(Role.ADMIN);
+  }
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
